51f262c9a50aa552a9ea587e2231bffec289c42d
Alina Beck <4ndroid4va@gmail.com>
2017-04-23 14:51:17 +0400

    feat(FakeAlert): adds fake alert screen

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import Router from '../containers/Router';
 
 import './app.css';
+import FakeAlert from './FakeAlert.js';
 
 class AppComponent extends React.Component {
 
   render() {
     return (
-      <Router />
+      <FakeAlert />
     );
   }
 }
---
+++ src/components/FakeAlert.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Fab from './Fab.js';
+import './issuealert.less';
+import './treeprofile.less';
+import './fab.less';
+
+import IconDone from '../images/IconDone';
+
+class FakeAlert extends React.Component {
+
+  render() {
+    return (
+      <main className="alert__background alert__background--fake">
+        <article className="profile__box profile__box--fake">
+          <h2 className="profile__section-title">A tree you care about is in danger!</h2>
+        </article>
+        <button type="button" className="cta fab--center fab--fake" onClick={() => this.props.sendAlert(this.refs.alertVal.value)} >Go!</button>
+      </main>
+    );
+  }
+}
+
+export default FakeAlert;
