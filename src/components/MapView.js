a4a323ca9c1042a470a5b23096ad67150e2126c5
Alina Beck <4ndroid4va@gmail.com>
2017-04-23 11:13:17 +0400

    feat(design): adds custom markers and icons

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -18,13 +18,18 @@ class MapView extends React.Component {
     if (nextProps.trees.length !== this.props.markers.length) {
       const markers = [];
       nextProps.trees.forEach(tree => {
+        const icon = {
+          url: `images/marker-${tree.status}.png`,
+          scaledSize: new google.maps.Size(10, 10)
+        }
         const marker = new google.maps.Marker({
           position: tree.position,
           map: this.map,
           draggable: false,
           animation: google.maps.Animation.DROP,
           name: tree.name,
-          id: tree.id
+          id: tree.id,
+          icon
         });
         marker.addListener('click', () => {
           that.props.openTree(marker.id);
